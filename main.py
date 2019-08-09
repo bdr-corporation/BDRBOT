c4b921908e726064e6e54362d4e6b17336d05ed0
alfdkfenfpgkrry0505 <51285169+alfdkfenfpgkrry0505@users.noreply.github.com>
2019-08-09 19:40:37 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
 #Work with Python 3.7.3
-import asyncio, discord, datetime, logging, random, traceback, time, os
-from discord.ext import commands
+import discord
+import asyncio
+import datetime
+import logging
+import random
+import traceback
+import time
+import os
+import bs4
+import urllib
+import urllib.request
+from urllib.request import urlopen, Request
 
 app = discord.Client()
 
@@ -76,6 +86,8 @@ async def on_message(message):
         embed = discord.Embed(title=" !볼 만한 영화 추천 ", description=" 배돌이가 제일 괜찮은 영화들을 한편 소개해줍니다 ", color=0xff0000)
         await message.channel.send(embed=embed)
         embed = discord.Embed(title=" !오늘의운세", description=" 배돌이가 오늘의운세를 짧고 간결하게 알려줍니다 ", color=0xff0000)
+        await message.channel.send(embed=embed)
+        embed = discord.Embed(title=" !실시간검색어", description=" 배돌이가 네이버 실시간 검색어 순위를 기반으로 현재 실검 현황을 알려줍니다 ", color=0xff0000)
         await message.channel.send(embed=embed)
         embed = discord.Embed(title=" !패치노트 ", description=" 배돌이가 자신의 패치노트를 불러옵니다 ", color=0xff0000)
         await message.channel.send(embed=embed)
@@ -230,6 +242,8 @@ async def on_message(message):
         embed = discord.Embed(title=" *0.1.2 ", description=" 2019년 07월 28일 일요일, 배돌이가 영화 한편을 무작위로 추천해줄수 있습니다, 지금 당장 추천받아보세요! ", color=0x00fefe)
         await message.channel.send(embed=embed)
         embed = discord.Embed(title=" *0.1.3 ", description=" 2019년 07월 28일 일요일, 배돌이가 오늘의운세를 알려줍니다. 지금 당장 운세를 무료로 받아보세요!", color=0x00fefe)
+        await message.channel.send(embed=embed)
+        embed = discord.Embed(title=" *0.1.4 ", description=" 2019년 08월 09일 금요일, 배돌이가 네이버 기반인 현 실시간 검색어 랭킹을 알려줍니다!", color=0x00fefe)
         await message.channel.send(embed=embed)
         embed = discord.Embed(title=" ", description=" ***패치노트는 계속 업데이트 할 예정입니다 ^00^ ", color=0x00fefe)
         await message.channel.send(embed=embed)
@@ -539,6 +553,32 @@ async def on_message(message):
         await message.channel.send("다양한 관계를 형성하는 만큼 깊이가 없다면 정작 당신이 힘들거나 도움이 필요한 순간에 당신이 속을 터놓고 고민을 말 하거나")
         await message.channel.send("혹은 도움을 요청할 만한 사람이 없다는 것을 의미하기도 합니다. ")
         await message.channel.send("때문에 항상 대인관계에 있어서 인내하고 배려하는 태도가 필요하다는 것을 잊지 마시고 모든 인간관계에 신중을 기하도록 하심이 좋습니다.")
+
+    if message.content.startswith('!실시간검색어') or message.content.startswith('!실검'):
+        url = "https://www.naver.com/"
+        html = urllib.request.urlopen(url)
+    
+        bsObj = bs4.BeautifulSoup(html, "html.parser")
+        realTimeSerach1 = bsObj.find('div', {'class': 'ah_roll_area PM_CL_realtimeKeyword_rolling'})
+        realTimeSerach2 = realTimeSerach1.find('ul', {'class': 'ah_l'})
+        realTimeSerach3 = realTimeSerach2.find_all('li')
+    
+    
+        embed = discord.Embed(
+        title='네이버 실시간 검색어',
+        description='실시간검색어',
+        color=discord.Color.green()
+            )
+        for i in range(0,20):
+            realTimeSerach4 = realTimeSerach3[i]
+            realTimeSerach5 = realTimeSerach4.find('span', {'class': 'ah_k'})
+            realTimeSerach = realTimeSerach5.text.replace(' ', '')
+            realURL = 'https://search.naver.com/search.naver?ie=utf8&query='+realTimeSerach
+            print(realTimeSerach)
+            embed.add_field(name=str(i+1)+'위', value='\n'+'[%s](<%s>)' % (realTimeSerach, realURL), inline=False) 
+    
+        await message.channel.send(embed=embed)       
+
 
 
 
