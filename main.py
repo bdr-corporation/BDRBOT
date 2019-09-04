1ca1c3fc259f77cc9e1b389c759443d6767d8f9a
alfdkfenfpgkrry0505 <51285169+alfdkfenfpgkrry0505@users.noreply.github.com>
2019-09-04 17:19:40 +0900

    Update main.py

--- main.py
+++ main.py
@@ -663,39 +663,85 @@ async def on_message(message):
         await message.channel.send(embed=embed)
         
         
-    if message.content.startswith('!사진'):
-        
-        Text = ""
+    if message.content.startswith("!오늘의날씨"):
         learn = message.content.split(" ")
-        vrsize = len(learn)  
-        vrsize = int(vrsize)
-        for i in range(1, vrsize):  
-            Text = Text + " " + learn[i]
-        print(Text.strip())  
-
-        randomNum = random.randrange(0, 40)
-
-        location = Text
-        enc_location = urllib.parse.quote(location) 
+        location = learn[1]
+        enc_location = urllib.parse.quote(location+'날씨')
         hdr = {'User-Agent': 'Mozilla/5.0'}
-        url = 'https://search.naver.com/search.naver?where=image&sm=tab_jum&query=' + enc_location 
+        url = 'https://search.naver.com/search.naver?where=nexearch&sm=top_hty&fbm=1&ie=utf8&query=' + enc_location
         print(url)
         req = Request(url, headers=hdr)
         html = urllib.request.urlopen(req)
-        bsObj = bs4.BeautifulSoup(html, "html.parser") 
-        
-        imgfind1 = bsObj.find('div', {'class': 'photo_grid _box'}) 
-        
-        imgfind2 = imgfind1.findAll('a', {'class': 'thumb _thumb'}) 
-        imgfind3 = imgfind2[randomNum]  
-        imgfind4 = imgfind3.find('img') 
-        imgsrc = imgfind4.get('data-source') 
-        print(imgsrc)
+        bsObj = bs4.BeautifulSoup(html, "html.parser")
+        todayBase = bsObj.find('div', {'class': 'main_info'})
+
+        todayTemp1 = todayBase.find('span', {'class': 'todaytemp'})
+        todayTemp = todayTemp1.text.strip()  
+        print(todayTemp)
+
+        todayValueBase = todayBase.find('ul', {'class': 'info_list'})
+        todayValue2 = todayValueBase.find('p', {'class': 'cast_txt'})
+        todayValue = todayValue2.text.strip()  
+        print(todayValue)
+
+        todayFeelingTemp1 = todayValueBase.find('span', {'class': 'sensible'})
+        todayFeelingTemp = todayFeelingTemp1.text.strip()  
+        print(todayFeelingTemp)
+
+        todayMiseaMongi1 = bsObj.find('div', {'class': 'sub_info'})
+        todayMiseaMongi2 = todayMiseaMongi1.find('div', {'class': 'detail_box'})
+        todayMiseaMongi3 = todayMiseaMongi2.find('dd')
+        todayMiseaMongi = todayMiseaMongi3.text  
+        print(todayMiseaMongi)
+
+        tomorrowBase = bsObj.find('div', {'class': 'table_info weekly _weeklyWeather'})
+        tomorrowTemp1 = tomorrowBase.find('li', {'class': 'date_info'})
+        tomorrowTemp2 = tomorrowTemp1.find('dl')
+        tomorrowTemp3 = tomorrowTemp2.find('dd')
+        tomorrowTemp = tomorrowTemp3.text.strip()  
+        print(tomorrowTemp)
+
+        tomorrowAreaBase = bsObj.find('div', {'class': 'tomorrow_area'})
+        tomorrowMoring1 = tomorrowAreaBase.find('div', {'class': 'main_info morning_box'})
+        tomorrowMoring2 = tomorrowMoring1.find('span', {'class': 'todaytemp'})
+        tomorrowMoring = tomorrowMoring2.text.strip() 
+        print(tomorrowMoring)
+
+        tomorrowValue1 = tomorrowMoring1.find('div', {'class': 'info_data'})
+        tomorrowValue = tomorrowValue1.text.strip()  
+        print(tomorrowValue)
+
+        tomorrowAreaBase = bsObj.find('div', {'class': 'tomorrow_area'})
+        tomorrowAllFind = tomorrowAreaBase.find_all('div', {'class': 'main_info morning_box'})
+        tomorrowAfter1 = tomorrowAllFind[1]
+        tomorrowAfter2 = tomorrowAfter1.find('p', {'class': 'info_temperature'})
+        tomorrowAfter3 = tomorrowAfter2.find('span', {'class': 'todaytemp'})
+        tomorrowAfterTemp = tomorrowAfter3.text.strip()  
+        print(tomorrowAfterTemp)
+
+        tomorrowAfterValue1 = tomorrowAfter1.find('div', {'class': 'info_data'})
+        tomorrowAfterValue = tomorrowAfterValue1.text.strip()
+
+        print(tomorrowAfterValue)  
+
         embed = discord.Embed(
-            color=discord.Color.blue()
+            title=learn[1]+ ' 오늘의 날씨 정보입니다.',
+            description=learn[1]+ '',
+            color=discord.Color.gold()
         )
-        embed.add_field(name='검색 : '+Text, value='링크 : '+imgsrc, inline=False)
-        embed.set_image(url=imgsrc) 
+        embed.add_field(name='현재온도', value=todayTemp+'˚', inline=False)  
+        embed.add_field(name='체감온도', value=todayFeelingTemp, inline=False) 
+        embed.add_field(name='현재상태', value=todayValue, inline=False)  
+        embed.add_field(name='현재 미세먼지 상태', value=todayMiseaMongi, inline=False)  
+        embed.add_field(name='오늘 오전/오후 날씨', value=tomorrowTemp, inline=False)  
+        embed.add_field(name='**----------------------------------**',value='**----------------------------------**', inline=False) 
+        embed.add_field(name='내일 오전온도', value=tomorrowMoring+'˚', inline=False)  
+        embed.add_field(name='내일 오전날씨상태, 미세먼지 상태', value=tomorrowValue, inline=False)  
+        embed.add_field(name='내일 오후온도', value=tomorrowAfterTemp + '˚', inline=False)  
+        embed.add_field(name='내일 오후날씨상태, 미세먼지 상태', value=tomorrowAfterValue, inline=False)  
+
+
+
         await message.channel.send(embed=embed)
  
 
